a512fe3323cecd345cc30b4466a683deab9e76d1
Tomáš <to.tichy@gmail.com>
2021-10-23 23:45:26 +0200

    update menu-items to navigetewitRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import sectionsData from "../../data.js";
+import sectionsData from "../../data/data.js";
 import MenuItem from "../menu-item/menu-item.component.jsx";
 
 import "./directory.styles.scss";
@@ -15,8 +15,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
